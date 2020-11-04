8dfa47848cfd7fe1eb9a1c24aafd9ef4b5f6c466
Luke Oleson <luke.oleson@flyt.io>
2020-11-03 16:07:43 -0800

    added an immatterial change to test CI

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>(...and Welcome to CD)</h2>
+    <h3>...yet another immaterial change...</h3>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
